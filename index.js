3e566b5f5020073f94d666d8476b973535f48869
Johan Kjölhede <johan.kjolhede@softwarelse.se>
2019-11-02 21:16:21 +0100

    WIP on converting back to fs

--- index.js
+++ index.js
@@ -12,30 +12,45 @@ const verbose = cmdLine.verbose;
 async function main() {
 
     let input = cmdLine.input;
-    if (cmdLine.output) {
-        throw new Error("Converting back not yet implemented!");
-    }
+    let output = cmdLine.output;
 
     if (input.endsWith("/")) {
         input = input.slice(0, input.length - 1)
     }
 
-    if (fs.existsSync(input)) {
-        const stat = fs.lstatSync(input);
-        if (stat.isDirectory()) {
-            console.error("Treating " + input + " as file system input, and trying to output its json representation");
-            const result = fs2JsObj(input);
-            console.log(JSON.stringify(result));
-        } else {
-            throw new Error("Unsupported input. Must be directory!");
-        }
-    } else {
+    if (output && output.endsWith("/")) {
+        output = output.slice(0, output.length - 1)
+    }
+
+    if (!fs.existsSync(input)) {
         throw new Error("Source file/folder '" + input + "' not found!");
+    }
+
+    if (output) {
+
+        if (fs.existsSync(output)) {
+            throw new Error("Target file/folder '" + output + "' already exists!");
+        }
+
+        if (verbose) {
+            console.error("Treating " + input + " as json file, and trying to write back the original file structure!");
+        }
+
+        const obj = JSON.parse(fs.readFileSync(cmdLine.input).toString());
+        ojb2Fs(obj)
+    }
+    else {
+
+        if (verbose) {
+            console.error("Treating " + input + " as file system input, and trying to output its json representation");
+        }
+
+        const result = fs2obj(input);
+        console.log(JSON.stringify(result));
     }
 }
 
-function fs2JsObj(path) {
-
+function fs2obj(path) {
 
     const stat = fs.lstatSync(path);
     const type = getTypeName(stat);
@@ -60,7 +75,94 @@ function fs2JsObj(path) {
                 console.error(path + " (dir)")
             }
 
-            result.children = fs.readdirSync(path).map(child => fs2JsObj(path + "/" + child));
+            result.children = fs.readdirSync(path).map(child => fs2obj(path + "/" + child));
+            break;
+        case "file":
+
+            const size = stat.size;
+            if (verbose) {
+                console.error(path + " (file, " + prettyBytes(size) + ")");
+            }
+
+            const data = fs.readFileSync(path);
+            let isText = istextorbinary.isTextSync(path, data);
+
+            result.encoding = isText ? "text" : "binary";
+            result.contents = isText ? data.toString() : data.toString('base64');
+            result.size = size;
+            break;
+        default:
+            console.error("WARN: Ignoring " + path + ", since it is of unsupported type: " + type);
+            break;
+    }
+
+    return result;
+}
+
+function ojb2Fs(obj, cwd = "", overridePath = null) {
+
+    const path = overridePath ? overridePath : (cwd === "" ? obj.path : cwd + "/" + obj.path);
+
+    switch(obj.type) {
+        case "symlink":
+
+            if (verbose) {
+                console.error(path + " (symlink)")
+            }
+
+            break;
+        case "dir":
+
+            if (verbose) {
+                console.error(path + " (dir)")
+            }
+
+            obj.children.map(child => ojb2Fs(child, path));
+
+            break;
+        case "file":
+
+            const size = obj.size;
+            if (verbose) {
+                console.error(path + " (file, " + prettyBytes(size) + ")");
+            }
+
+            const data = fs.readFileSync(path);
+            let isText = istextorbinary.isTextSync(path, data);
+
+            result.encoding = isText ? "text" : "binary";
+            result.contents = isText ? data.toString() : data.toString('base64');
+            result.size = size;
+            break;
+        default:
+            console.error("Cannot recreate object " + JSON.stringify(obj) + " because it is of unsupported type " + obj.type);
+            break;
+    }
+
+    const stat = fs.lstatSync(path);
+    const type = getTypeName(stat);
+    const result = {
+        type: getTypeName(stat),
+        path: path,
+        pathAbs: libPath.resolve(path)
+    };
+
+    switch (type) {
+        case "symlink":
+
+            if (verbose) {
+                console.error(path + " (symlink)")
+            }
+
+            result.target = fs.readlinkSync(path);
+            break;
+        case "dir":
+
+            if (verbose) {
+                console.error(path + " (dir)")
+            }
+
+            result.children = fs.readdirSync(path).map(child => fs2obj(path + "/" + child));
             break;
         case "file":
 
